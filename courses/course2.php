44dde37b8b6eae245f645f3ebc3cf7aa9b56d0c9
Mortell560 <mortell560@gmail.com>
2023-11-18 22:52:45 +0100

    spooky scary skeleton

---
+++ courses/course2.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>GT.com</title>
+        <link rel="icon" href="../images/GraphTheoryLogo.ico">
+        <link rel="stylesheet" href="../stylesheets/general_style.css">
+    </head>
+
+    <body>
+        <header>
+            <div class="logo">
+                <img src="../images/LogoCropped.png" alt="Logo">
+            </div>
+            <div class="navbar"> <!-- On utilisera div pour eviter le point generer par <ul> -->
+                <a href="../index.php">Home</a>
+                <a href="../courses.php">Cours</a>
+                <div class="dropdown">
+                    <button class="dropbtn">Exercises
+                    <i class="fa fa-caret-down"></i>
+                    </button>
+                    <div class="dropdown-content">
+                    <a href="../exercises.php">Liste des exercises</a>
+                    <a href="#">Ex1</a>
+                    <a href="#">Ex2</a>
+                    </div>
+                </div>
+                <a href="../projects.php">Projets</a>
+                <a href="../about.php">About</a>
+            </div>
+        </header>
+
+
+        <section>
+
+        </section>
+
+
+        <footer>
+            <i><p>GT.com Copyright (c)<br/>Your favorite graph theory website since 2023</p></i>
+        </footer>
+    </body>
+</html>
