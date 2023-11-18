5ea9ecf600ed5391fe603023759f3aad35f3f511
Mortell560 <mortell560@gmail.com>
2023-11-18 17:26:43 +0100

    Added images

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
 <html>
     <head>
-        <title>PHP Test</title>
+        <title>GT.com</title>
+        <link rel="stylesheet" href="stylesheets/general_style.css">
+        <link rel="stylesheet" href="stylesheets/style_index.css">
+        <link rel="icon" href="images/GraphTheoryLogo.ico">
     </head>
     <body>
-        <?php echo '<p>Hello World</p>'; ?>
+        <header>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="graphs.php">Graphs</a></li>
+                <li><a href="algorithms.php">Algorithms</a></li>
+                <li><a href="about.php">About</a></li>
+            </ul>
+
+        </header>
+
+        <section>
+
+
+        </section>
+
+
+        <footer>
+            <i><p>GT.com Copyright (c)<br/>Your favorite graph theory website since 2023</p></i>
+        </footer>
     </body>
 </html>
