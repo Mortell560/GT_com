545f62a9f4bf161c2f0dba0cb5479b5572905774
Mortell560 <mortell560@gmail.com>
2023-11-18 18:23:32 +0100

    index done (approx)

--- index.php
+++ index.php
@@ -3,14 +3,18 @@
         <title>GT.com</title>
         <link rel="stylesheet" href="stylesheets/general_style.css">
         <link rel="icon" href="images/GraphTheoryLogo.ico">
+        <meta charset="UTF-8">
     </head>
     <body>
         <header>
+            <div class="logo">
+                <img src="images/LogoCropped.png" alt="Logo">
+            </div>
             <div class="navbar"> <!-- On utilisera div pour eviter le point generer par <ul> -->
                 <a href="#home">Home</a>
                 <a href="#news">Cours</a>
                 <div class="dropdown">
-                    <button class="dropbtn">Dropdown
+                    <button class="dropbtn">Exercises
                     <i class="fa fa-caret-down"></i>
                     </button>
                     <div class="dropdown-content">
@@ -26,8 +30,15 @@
         </header>
 
         <section>
-
-
+            <article>
+                <h1>La théorie des graphes</h1>
+                <div class="img_source">
+                    <img src="images/Giftgrafos.gif" alt="Graph gif">
+                    <p>Source: <a href="https://commons.wikimedia.org/wiki/Category:Animations_of_graphs_(graph_theory)#/media/File:Giftgrafos.gif">https://commons.wikimedia.org/wiki/Category:Animations_of_graphs_(graph_theory)#/media/File:Giftgrafos.gif</a></p>
+                </div>
+                <p>La théorie des graphes est la discipline mathématique et informatique qui étudie les graphes, lesquels sont des modèles abstraits de dessins de réseaux reliant des objets. Ces modèles sont constitués par la donnée de sommets (aussi appelés nœuds ou points, en référence aux polyèdres), et d'arêtes (aussi appelées liens ou lignes) entre ces sommets.<br>Ces arêtes sont parfois non symétriques (les graphes sont alors dits orientés) et sont alors appelées des flèches ou des arcs.</p>
+                <p>Veuillez selectionner une entrée du site dans la barre de navigation pour commencer.</p>
+            </article>
         </section>
 
 
