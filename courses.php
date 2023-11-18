957b8fcc5c87ca18d206b25348e75701072d8324
Mortell560 <mortell560@gmail.com>
2023-11-18 22:09:39 +0100

    started skeleton for courses

---
+++ courses.php
@@ -0,0 +1,49 @@
+<html>
+    <head>
+        <title>GT.com</title>
+        <link rel="stylesheet" href="stylesheets/general_style.css">
+        <link rel="icon" href="images/GraphTheoryLogo.ico">
+        <link rel="stylesheet" href="stylesheets/courseList_style.css">
+        <meta charset="UTF-8">
+    </head>
+
+    <body>
+        <header>
+            <div class="logo">
+                <img src="images/LogoCropped.png" alt="Logo">
+            </div>
+            <div class="navbar"> <!-- On utilisera div pour eviter le point generer par <ul> -->
+                <a href="index.php">Home</a>
+                <a href="courses.php">Cours</a>
+                <div class="dropdown">
+                    <button class="dropbtn">Exercises
+                    <i class="fa fa-caret-down"></i>
+                    </button>
+                    <div class="dropdown-content">
+                    <a href="exercises.php">Liste des exercises</a>
+                    <a href="#">Ex1</a>
+                    <a href="#">Ex2</a>
+                    </div>
+                </div>
+                <a href="projects.php">Projets</a>
+                <a href="about.php">About</a>
+            </div>
+        </header>
+
+        <section>
+            <article>
+                <h1>Choissiez votre cours</h1>
+                <ol class="selection">
+                    <li><a href="courses/course1.php">Histoire de la Théorie des Graphes</a></li>
+                    <li><a href="courses/course2.php">Les Graphes: Définitions et notions formelles</a></li>
+                    <li><a href="courses/course3.php">Les Graphes: Théorèmes et propriétés utiles</a></li>
+                    <li><a href="courses/course4.php">Quelques structures de données utiles et implémentations</a></li>
+                </ol>
+            </article>
+        </section>
+
+        <footer>
+            <i><p>GT.com Copyright (x)<br/>Your favorite graph theory website since 2023</p></i>
+        </footer>
+    </body>
+</html>
---
+++ courses/course1.php
@@ -0,0 +1,49 @@
+<html>
+    <head>
+        <title>GT.com</title>
+        <link rel="stylesheet" href="../stylesheets/general_style.css">
+        <link rel="icon" href="../images/GraphTheoryLogo.ico">
+        <meta charset="UTF-8">
+    </head>
+
+    <body>
+        <header>
+            <div class="logo">
+                <img src="../images/LogoCropped.png" alt="Logo">
+            </div>
+            <div class="navbar"> <!-- On utilisera div pour eviter le point generer par <ul> -->
+                <a href="../index.php">Home</a>
+                <a href="../courses.php">Cours</a>
+                <div class="dropdown">
+                    <button class="dropbtn">Exercises
+                    <i class="fa fa-caret-down"></i>
+                    </button>
+                    <div class="dropdown-content">
+                    <a href="../exercises.php">Liste des exercises</a>
+                    <a href="#">Ex1</a>
+                    <a href="#">Ex2</a>
+                    </div>
+                </div>
+                <a href="../projects.php">Projets</a>
+                <a href="../about.php">About</a>
+            </div>
+        </header>
+        
+        <section>
+            <article>
+                <h1>
+                    L'histoire de la Théorie des Graphes
+                </h1>
+
+                <p>Un article du mathématicien suisse Leonhard Euler, présenté à l'Académie de Saint-Pétersbourg en 1735 puis publié en 1741, traitait du problème des sept ponts de Königsberg.<br>Son énoncé est le suivant:</p>
+                <blockquote cite="https://archive.wikiwix.com/cache/index2.php?url=http%3A%2F%2Feulerarchive.maa.org%2Fdocs%2Foriginals%2FE053.pdf%2Findex.html#federation=archive.wikiwix.com&tab=url">
+                La ville de Königsberg (aujourd'hui Kaliningrad) est construite autour de deux îles situées sur le Pregel et reliées entre elles par un pont. Six autres ponts relient les rives de la rivière à l'une ou l'autre des deux îles, comme représentés sur le plan ci-dessus. Le problème consiste à déterminer s'il existe ou non une promenade dans les rues de Königsberg permettant, à partir d'un point de départ au choix, de passer une et une seule fois par chaque pont, et de revenir à son point de départ, étant entendu qu'on ne peut traverser le Pregel qu'en passant sur les ponts.<br>Le problème mathématique se présente de la façon suivante :
+                </blockquote>
+            </article>
+        </section>
+
+        <footer>
+            <i><p>GT.com Copyright (c)<br/>Your favorite graph theory website since 2023</p></i>
+        </footer>
+    </body>
+</html>
