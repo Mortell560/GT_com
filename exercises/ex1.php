1edcd557c1f3fd18e5d35d2d4f1b1e8279836ae4
Mortell560 <mortell560@gmail.com>
2023-11-19 15:57:00 +0100

    ex1.php working

--- exercises/ex1.php
+++ exercises/ex1.php
@@ -15,17 +15,7 @@
             <div class="navbar"> <!-- On utilisera div pour eviter le point generer par <ul> -->
                 <a href="../index.php">Home</a>
                 <a href="../courses.php">Cours</a>
-                <div class="dropdown">
-                    <button class="dropbtn">Exercices
-                    <i class="fa fa-caret-down"></i>
-                    </button>
-                    <div class="dropdown-content">
-                    <a href="../exercises.php">Liste des Exercices</a>
-                    <a href="ex1.php">Ex facile</a>
-                    <a href="ex2.php">Ex Moyen</a>
-                    <a href="ex3.php">Ex "Hard"</a>
-                    </div>
-                </div>
+                <a href="../exercises.php">Exercices</a>
                 <a href="../projects.php">Projets</a>
                 <a href="../about.php">About</a>
             </div>
@@ -34,7 +24,8 @@
         <nav class="sidenav">
             <a href="ex1.php">Exercice (Facile)</a>
             <ul>
-                <li><a href=""></a></li>
+                <li><a href="#BFS_DFS">Parcours en largueur et en longueur</a></li>
+                <li><a href="#Ex1">Exercice</a></li>
             </ul>
             <a href="ex2.php">Exercice (Moyen)</a>
             <ul>
@@ -50,7 +41,7 @@
             <article>
                 <h1>Commençons avec les bases:</h1>
 
-                <h2>
+                <h2 id="BFS_DFS">
                     1) Parcours en largeur et en longueur (BFS et DFS en anglais)
                 </h2>
 
@@ -58,6 +49,10 @@
                     Pour réaliser un parcours en largeur, il faut utiliser une file (FIFO) et pour un parcours en profondeur, il faut utiliser une pile (LIFO).<br/>
                     <ul>
                         <li>Parcours en largeur: on commence par explorer un nœud source, puis ses successeurs, puis les successeurs non explorés des successeurs, etc.</li>
+                        <div class="img_source">
+                            <img src="../images/Breadth-First-Search-Algorithm.gif" alt="DFS">
+                            <p>Source: <a href="https://commons.wikimedia.org/wiki/File:Breadth-First-Search-Algorithm.gif"> BFS (Wikipedia)</a></p>
+                        </div>
                         <li>Parcours en profondeur: Il poursuit alors un chemin dans le graphe jusqu'à un cul-de-sac ou alors jusqu'à atteindre un sommet déjà visité. Il revient alors sur le dernier sommet où on pouvait suivre un autre chemin puis explore un autre chemin</li>
                         <div class="img_source">
                             <img src="../images/Depth-First-Search.gif" alt="DFS">
@@ -65,6 +60,25 @@
                         </div>
                     </ul>
                 </p>
+
+                <h2 id="Ex1">
+                    2) Exercice
+                </h2>
+                <p>Donnez le parcours en largeur et en profondeur du graphe ci-dessous (On commencera par le sommet "1") en utilisant un programme fait dans le langage de votre choix.</p>
+                <img src="../images/GraphEx1.png" alt="GraphEX1">
+                <br>
+                <fieldset>
+                    <legend>
+                        Entrez vos réponses ici
+                    </legend>
+                    <form action="../exercises_corr/ex1_corr.php" method="post">
+                        <label for="BFS">Parcours en largeur:</label>
+                        <input type="text" name="BFS" id="BFS" placeholder="Ex: 1,2,3,4,5,6,7,8,9,10"><br>
+                        <label for="DFS">Parcours en profondeur:</label>
+                        <input type="text" name="DFS" id="DFS" placeholder="Ex: 1,2,3,4,5,6,7,8,9,10"><br>
+                        <input type="submit" value="Valider">
+                    </form>
+                </fieldset>
 
             </article>
         </section>
--- exercises_corr/ex1_corr.php
+++ exercises_corr/ex1_corr.php
@@ -2,6 +2,7 @@
     <head>
         <title>GT.com</title>
         <link rel="stylesheet" href="../stylesheets/general_style.css">
+        <link rel="stylesheet" href="../stylesheets/exercice_style.css">
         <link rel="icon" href="../images/GraphTheoryLogo.ico">
         <meta charset="utf-8">
     </head>
@@ -31,8 +32,87 @@
         </header>
 
         <section>
+            <nav class="sidenav">
+                <a href="../exercises/ex1.php">Exercice (Facile)</a>
+                <ul>
+                    <li><a href="#BFS_DFS">Parcours en largueur et en longueur</a></li>
+                    <li><a href="#Ex1">Exercice</a></li>
+                </ul>
+                <a href="../exercises/ex2.php">Exercice (Moyen)</a>
+                <ul>
+                    <li><a href=""></a></li>
+                </ul>
+                <a href="../exercises/ex3.php">Exercice (Difficile)</a>
+                <ul>
+                    <li><a href=""></a></li>
+                </ul>
+            </nav>
             <article>
+                <h1>Commençons avec les bases:</h1>
+                <h2 id="BFS_DFS">
+                    1) Parcours en largeur et en longueur (BFS et DFS en anglais)
+                </h2>
 
+                <p>
+                    Pour réaliser un parcours en largeur, il faut utiliser une file (FIFO) et pour un parcours en profondeur, il faut utiliser une pile (LIFO).<br/>
+                    <ul>
+                        <li>Parcours en largeur: on commence par explorer un nœud source, puis ses successeurs, puis les successeurs non explorés des successeurs, etc.</li>
+                        <div class="img_source">
+                            <img src="../images/Breadth-First-Search-Algorithm.gif" alt="DFS">
+                            <p>Source: <a href="https://commons.wikimedia.org/wiki/File:Breadth-First-Search-Algorithm.gif"> BFS (Wikipedia)</a></p>
+                        </div>
+                        <li>Parcours en profondeur: Il poursuit alors un chemin dans le graphe jusqu'à un cul-de-sac ou alors jusqu'à atteindre un sommet déjà visité. Il revient alors sur le dernier sommet où on pouvait suivre un autre chemin puis explore un autre chemin</li>
+                        <div class="img_source">
+                            <img src="../images/Depth-First-Search.gif" alt="DFS">
+                            <p>Source: <a href="https://en.m.wikipedia.org/wiki/File:Depth-First-Search.gif"> DFS (Wikipedia)</a></p>
+                        </div>
+                    </ul>
+                </p>
+
+                <h2 id="Ex1">
+                    2) Exercice
+                </h2>
+                <p>Donnez le parcours en largeur et en profondeur du graphe ci-dessous (On commencera par le sommet "1") en utilisant un programme fait dans le langage de votre choix.</p>
+                <img src="../images/GraphEx1.png" alt="GraphEX1">
+                <br>
+                <fieldset>
+                    <?php
+                        $corr = 0;
+                        if(isset($_POST['BFS']))
+                        {
+                            $BFS = $_POST['BFS'];
+                            $BFS = str_replace(' ','',$BFS);
+                            $BFS_corr = "1,2,6,3,4,5,7";
+                            if ($BFS === $BFS_corr)
+                            {
+                                $corr++;
+                            }
+                            else{
+                                echo "<p>Le parcours en largeur est faux</p>";
+                            }
+                        }
+                        if (isset($_POST['DFS']))
+                        {
+                            $DFS = $_POST['DFS'];
+                            $DFS = str_replace(' ','',$DFS);
+                            $DFS_corr = "1,2,3,4,6,5,7";
+                            if ($DFS === $DFS_corr)
+                            {
+                                $corr++;
+                            }
+                            else{
+                                echo "<p>Le parcours en profondeur est faux</p>";
+                            }
+                        }
+                        if ($corr == 2)
+                        {
+                            echo "<legend style=\"color: green;\">Bravo, vous avez réussi l'exercice</legend>";
+                        }
+                        else{
+                            echo "<legend style=\"color: red;\">Vous avez fait $corr/2</legend>";
+                        }
+                    ?>
+                </fieldset>
             </article>
         </section>
         
