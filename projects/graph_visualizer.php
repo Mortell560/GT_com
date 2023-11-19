36f22b62865394c8d3c2a263407f198240fb3aaa
Mortell560 <mortell560@gmail.com>
2023-11-19 01:11:09 +0100

    started projects

--- projects/graph_visualizer.php
+++ projects/graph_visualizer.php
@@ -1,47 +1,43 @@
 <html>
     <head>
         <title>GT.com</title>
-        <link rel="stylesheet" href="stylesheets/general_style.css">
-        
-        <link rel="icon" href="images/GraphTheoryLogo.ico">
+        <link rel="stylesheet" href="../stylesheets/general_style.css">
+        <link rel="icon" href="../images/GraphTheoryLogo.ico">
         <meta charset="utf-8">
     </head>
 
     <body>
         <header>
             <div class="logo">
-                <img src="images/LogoCropped.png" alt="Logo">
+                <img src="../images/LogoCropped.png" alt="Logo">
             </div>
             <div class="navbar"> <!-- On utilisera div pour eviter le point generer par <ul> -->
-                <a href="index.php">Home</a>
-                <a href="courses.php">Cours</a>
+                <a href="../index.php">Home</a>
+                <a href="../courses.php">Cours</a>
                 <div class="dropdown">
                     <button class="dropbtn">Exercises
                     <i class="fa fa-caret-down"></i>
                     </button>
                     <div class="dropdown-content">
-                    <a href="exercises.php">Liste des exercises</a>
+                    <a href="../exercises.php">Liste des exercises</a>
                     <a href="#">Ex1</a>
                     <a href="#">Ex2</a>
                     </div>
                 </div>
-                <a href="projects.php">Projets</a>
-                <a href="about.php">About</a>
+                <a href="../projects.php">Projets</a>
+                <a href="../about.php">About</a>
             </div>
         </header>
 
         <section>
             <article>
-                <h1 id="Title">Choisissez votre cours</h1>
-                <ol class="selection">
-                    <li><a href="courses/course1.php">Histoire de la Théorie des Graphes</a></li>
-                    <li><a href="courses/course2.php">Les Graphes: Définitions et notions formelles</a></li>
-                </ol>
+                
             </article>
         </section>
 
         <footer>
             <i><p>GT.com Copyright (c)<br/>Your favorite graph theory website since 2023</p></i>
         </footer>
+
     </body>
 </html>
