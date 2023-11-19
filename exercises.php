827e69e4537aa4ca538c8321334fe3faee6ce670
Mortell560 <mortell560@gmail.com>
2023-11-19 01:04:25 +0100

    :skull:

---
+++ exercises.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <title>GT.com</title>
+        <link rel="stylesheet" href="stylesheets/general_style.css">
+        <link rel="icon" href="images/GraphTheoryLogo.ico">
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+        <header>
+            <div class="logo">
+                <img src="images/LogoCropped.png" alt="Logo">
+            </div>
+            <div class="navbar"> <!-- On utilisera div pour eviter le point generer par <ul> -->
+                <a href="index.php">Home</a>
+                <a href="courses.php">Cours</a>
+                <div class="dropdown">
+                    <button class="dropbtn">Exercises
+                    <i class="fa fa-caret-down"></i>
+                    </button>
+                    <div class="dropdown-content">
+                    <a href="exercises.php">Liste des exercises</a>
+                    <a href="#">Ex1</a>
+                    <a href="#">Ex2</a>
+                    </div>
+                </div>
+                <a href="projects.php">Projets</a>
+                <a href="about.php">About</a>
+            </div>
+        </header>
+
+        <section>
+
+        </section>
+
+        <footer>
+            <i><p>GT.com Copyright (c)<br/>Your favorite graph theory website since 2023</p></i>
+        </footer>
+    </body>
+</html>
